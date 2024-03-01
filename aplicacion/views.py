bb0f1bebed0199ff0789466fed43aa477e51d329
JuanArcidiacono03 <arcidiacono.juan22@gmail.com>
2024-03-01 16:41:19 -0300

    Proyecto Inicial con Diseño

---
+++ aplicacion/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def home(request):
+    return render(request, "aplicacion/index.html")
+
+def cursos(request):
+    contexto = {'cursos': Curso.objects.all()}
+    return render(request, "aplicacion/cursos.html", contexto)
+
+def profesores(request):
+    return render(request, "aplicacion/profesores.html")
+
+def estudiantes(request):
+    return render(request, "aplicacion/estudiantes.html")
+
+def entregables(request):
+    return render(request, "aplicacion/entregables.html")
+
+
+
